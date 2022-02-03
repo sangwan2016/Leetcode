8a6dfa702776b349df9072b87da7ee29734cf43d
sangwan2016 <61751079+sangwan2016@users.noreply.github.com>
2022-02-03 19:47:46 +0900

    Create Binary Search.py

---
+++ Binary Search.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def search(self, nums, target):
+        l = 0
+        r = len(nums)-1
+        while True:
+            mid = (l+r)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                l = mid + 1
+            else:
+                r = mid - 1
+            if l > r:
+                return -1
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        
