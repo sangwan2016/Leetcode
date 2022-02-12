f03c6ab907d4da9390648d10dd145bd878e9bbad
sangwan2016 <61751079+sangwan2016@users.noreply.github.com>
2022-02-12 09:44:16 +0900

    Create Smallest Value of the Rearranged Number.py

---
+++ Smallest Value of the Rearranged Number.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def smallestNumber(self, num):
+        if num > 0:
+            digit_map = map(int, str(num))
+            digit_list = list(digit_map)
+            digit_list.sort()
+            if digit_list[0] == 0:
+                for i in range(len(digit_list)):
+                    if digit_list[i] != 0:
+                        digit_list[0] = digit_list[i]
+                        digit_list[i] = 0
+                        break
+            res = int("".join(map(str, digit_list)))
+            return res
+        else:
+            num = abs(num)
+            digit_map = map(int, str(num))
+            digit_list = list(digit_map)
+            digit_list.sort(reverse = True)
+            res = int("".join(map(str, digit_list)))
+            return -1*res
+        """
+        :type num: int
+        :rtype: int
+        """
+        
